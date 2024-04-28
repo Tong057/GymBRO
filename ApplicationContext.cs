91173b4f32264d138ad7c8aa9cc0013568526920
gnevilkoko <mazegora.gleb@gmail.com>
2024-04-28 21:11:06 +0200

    refactor

--- ApplicationContext.cs
+++ ApplicationContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,9 @@ using System.Threading.Tasks;
 
 namespace GymBro
 {
-    class ApplicationContext : DbContext
+    partial class ApplicationContext : ObservableObject
     {
+        [ObservableProperty]
+        private string pep;
     }
 }
