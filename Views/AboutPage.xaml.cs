eec6bb136b1d51b1de9dcbe2f8419829cbaf3ff5
Tong057 <tonykatolichenko@gmail.com>
2024-05-02 18:53:00 +0200

    About page has been enhnanced

--- Views/AboutPage.xaml.cs
+++ Views/AboutPage.xaml.cs
@@ -1,3 +1,5 @@
+using Microsoft.Maui.Controls;
+
 namespace GymBro.Views
 {
     public partial class AboutPage : ContentPage
@@ -5,6 +7,45 @@ namespace GymBro.Views
         public AboutPage()
         {
             InitializeComponent();
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            if (AppShell.Current.FlyoutIsPresented)
+            {
+                return false;
+            }
+            else
+            {
+                AppShell.Current.FlyoutIsPresented = true;
+                return true;
+            }
+        }
+
+        // It handles when the loading is starting
+        private void OnNavigating(object sender, WebNavigatingEventArgs e)
+        {
+            LoadingIndicator.IsVisible = true;
+            LoadingIndicator.IsRunning = true;
+
+            ErrorLayout.IsVisible = false;
+        }
+
+        // It handles when the loading is finished
+        private void OnNavigated(object sender, WebNavigatedEventArgs e)
+        {
+            LoadingIndicator.IsVisible = false;
+            LoadingIndicator.IsRunning = false;
+
+            if (e.Result == WebNavigationResult.Failure)
+            {
+                ErrorLayout.IsVisible = true;
+            }
+        }
+
+        private void OnReloadClicked(object sender, EventArgs e)
+        {
+            WebView.Reload();
         }
     }
 }
